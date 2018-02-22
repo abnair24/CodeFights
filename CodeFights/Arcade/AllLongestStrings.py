14ed7c51db6d3543cb094316984df2050d30c63f
Aswathy <aswathy.sreenath9@gmail.com>
2018-02-22 12:19:37 +0530

    Refactored all longest strings

--- CodeFights/Arcade/AllLongestStrings.py
+++ CodeFights/Arcade/AllLongestStrings.py
@@ -1,12 +1,29 @@
 class AllLongestStrings:
-    def __init__(self, inputArray):
-        self.inputArray = inputArray
+    def __init__(self, inplist):
+        self.inplist = inplist
 
-    def all_longest_strings(self):
-        max_length = 0
-        for i in range(0, len(self.inputArray)):
-            max_length = max(max_length, len(self.inputArray[i]))
-        return [i for i in self.inputArray if len(i) == max_length]
+    def alllongeststrings(self):
+        maxlen = len(self.inplist[0])
+        i = 0
+        newlist = []
+        if len(self.inplist) == 1:
+            newlist.append(self.inplist[0])
+        while i < len(self.inplist) - 1:
+            if maxlen == len(self.inplist[i + 1]):
+                if i == 0:
+                    newlist.append(self.inplist[i])
+                newlist.append(self.inplist[i + 1])
+            elif maxlen < len(self.inplist[i + 1]):
+                newlist.clear()
+                newlist.append(self.inplist[i + 1])
+                maxlen = len(self.inplist[i + 1])
+            else:
+                if i == 0:
+                    maxlen = len(self.inplist[i])
+                    newlist.append(self.inplist[i])
+            i += 1
+
+        return newlist
 
     @staticmethod
     def get_input():
@@ -17,13 +34,6 @@ class AllLongestStrings:
 if __name__ == '__main__':
     array = AllLongestStrings.get_input()
     all_longest_strings = AllLongestStrings(array)
-    print(all_longest_strings.all_longest_strings())
-
-
-
-
-
-
-
+    print(all_longest_strings.alllongeststrings())
 
 
