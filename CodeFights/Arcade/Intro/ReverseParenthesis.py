9467b26b816d26437706a2ed8b40e6d2f9422241
Aswathy <aswathy.sreenath9@gmail.com>
2018-06-09 15:43:32 +0530

    ReverseParenthesis

---
+++ CodeFights/Arcade/Intro/ReverseParenthesis.py
@@ -0,0 +1,15 @@
+def reverseParentheses(s):
+    for i in range(len(s)):
+        if s[i] == "(":
+            start = i
+        if s[i] == ")":
+            end = i
+            return reverseParentheses(s[:start] + s[start+1:end][::-1] + s[end+1:])
+    return s
+
+
+
+print(reverseParentheses("Code(Cha(lle)nge)"))
+
+
+
