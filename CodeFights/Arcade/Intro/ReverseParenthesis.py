4edf09c216e67ea86ea4b08a1574b61de1ae362d
Aswathy <aswathy.sreenath9@gmail.com>
2018-06-10 11:57:45 +0530

    Alternating sums

--- CodeFights/Arcade/Intro/ReverseParenthesis.py
+++ CodeFights/Arcade/Intro/ReverseParenthesis.py
@@ -1,15 +1,25 @@
-def reverseParentheses(s):
+# You have a string s that consists of English letters, punctuation marks,
+# whitespace characters, and brackets. It is guaranteed that the parentheses in s form a regular bracket sequence.
+#
+# Your task is to reverse the strings contained in each pair of matching parentheses,
+# starting from the innermost pair. The results string should not contain any parentheses.
+#
+# Example
+#
+# For string s = "a(bc)de", the output should be
+# reverseParentheses(s) = "acbde".
+
+
+def reverse_parentheses(s):
     for i in range(len(s)):
         if s[i] == "(":
             start = i
         if s[i] == ")":
             end = i
-            return reverseParentheses(s[:start] + s[start+1:end][::-1] + s[end+1:])
+            return reverse_parentheses(s[:start] + s[start+1:end][::-1] + s[end+1:])
     return s
 
 
-
-print(reverseParentheses("Code(Cha(lle)nge)"))
-
+print(reverse_parentheses("Code(Cha(lle)nge)"))
 
 
