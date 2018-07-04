cf452f3ba2b9479b56813ef051da144a26376b2d
Aswathy <aswathy.sreenath9@gmail.com>
2018-07-04 23:39:32 +0530

    KillKthBit

--- CodeFights/Arcade/TheCore/KillKthBit.py
+++ CodeFights/Arcade/TheCore/KillKthBit.py
@@ -1,13 +1,6 @@
 def killKthBit(n, k):
-    y = bin(n)[2:]
-    x = y[-(k)]
-    print(x)
-    if x == 1:
-        b1 = int(1 << (k-1))
-        print(b1)
-        return n ^ b1
-    else:
-        return n
+    print(bin(n))
+    return n if n == 0 or len(bin(n)) < k or bin(n)[2:][-k] == '0' else n ^ int(1 << (k - 1))
 
 
-print(killKthBit(37, 4))
+print(killKthBit( 2738, 30))
