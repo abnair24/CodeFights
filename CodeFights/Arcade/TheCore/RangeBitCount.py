6227f5abbb6330051b42384d518e41903a7e77b9
Aswathy <aswathy.sreenath9@gmail.com>
2018-07-06 15:10:00 +0530

    SecondRightMostZeroBit

--- CodeFights/Arcade/TheCore/RangeBitCount.py
+++ CodeFights/Arcade/TheCore/RangeBitCount.py
@@ -1,38 +1,4 @@
-You
-are
-given
-two
-numbers
-a and b
-where
-0 ≤ a ≤ b.Imagine
-you
-construct
-an
-array
-of
-all
-the
-integers
-from a to
 
-b
-inclusive.You
-need
-to
-count
-the
-number
-of
-1
-s in the
-binary
-representations
-of
-all
-the
-numbers in the
-array.
 
 # Example
 #
