dd56aa2214e0a42deda554356a41fc1de189d51a
Aswathy <aswathy.sreenath9@gmail.com>
2018-07-07 16:12:22 +0530

    CountSumOfTwoRepresentations2

--- CodeFights/Arcade/TheCore/CountSumOfTwoRepresentations2.py
+++ CodeFights/Arcade/TheCore/CountSumOfTwoRepresentations2.py
@@ -1,8 +1,17 @@
+# Given integers n, l and r, find the number of ways to represent n as a sum of two integers A and B such that l ≤ A ≤ B ≤ r.
+#
+# Example
+#
+# For n = 6, l = 2, and r = 4, the output should be
+# countSumOfTwoRepresentations2(n, l, r) = 2.
+#
+# There are just two ways to write 6 as A + B, where 2 ≤ A ≤ B ≤ 4: 6 = 2 + 4 and 6 = 3 + 3.
+
+
 def countSumOfTwoRepresentations2(n, l, r):
 
     # return len ({ frozenset(p) for p in [(i, n-i) for i in range(l,r+1) if l <= n-i <= r ] })
     return len({tuple(sorted(p)) for p in [(i, n-i) for i in range(l, r+1) if l <= n-i <= r]})
-
 
 
 print(countSumOfTwoRepresentations2(6,2,4))
